54905ddc3a1089e00060590b568b6a610e5ac434
shufflesninja <isaact23@vt.edu>
2022-05-23 11:53:26 -0400

    Add basic variables and objective function

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ from words import WORDS
 # Define crossword width and height
 SIZE = 9
 
+
 def gen():
-    # 10 qubits
-    x = [dimod.Binary(f'x_{i}') for i in range(10)]
     cqm = dimod.ConstrainedQuadraticModel()
 
-    # Objective: as many 1s as possible.
-    cqm.set_objective(-sum(x))
+    # o_i,j is whether a square is occupied (has a letter).
+    o = []
+    for i in range(SIZE):
+        for j in range(SIZE):
+            o[i][j * SIZE] = dimod.Binary(f'o_{i},{j}')
 
-    # Constraint: no more than 3 1s.
-    cqm.add_constraint(sum(x) <= 3, label='max_sum')
+    # x_i,j is the letter in row i and col j.
+    l = []
+    for i in range(SIZE):
+        for j in range(SIZE):
+            l[i][j * SIZE] = dimod.Integer(f'l_{i},{j}')
 
-    # Solve
-    solver = LeapHybridCQMSampler()
-    result = solver.sample_cqm(cqm)
-    print(result)
+    # Objective: fill as many squares as possible with letters.
+    cqm.set_objective(-sum(o))
 
 
 if __name__ == '__main__':
